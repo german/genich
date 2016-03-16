66fb505ce6ec85f960bceb1b3d514f5552288ce7
Dmytro Samoilov <e2718281828@ya.ru>
2016-03-16 18:28:53 +0200

    added 5 test seed hotels

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Hotel.delete_all
+Hotel.create({name: "Кобзарь", city_name: 'Геническ', dist_from_the_sea: 200, has_tv: true})
+Hotel.create({name: "Источник здоровья", city_name: 'Генгорка', dist_from_the_sea: 500, has_tv: true})
+Hotel.create({name: "Элит", city_name: 'Счастливцево', dist_from_the_sea: 50, has_tv: true})
+Hotel.create({name: "Комфорт Плюс", city_name: 'Счастливцево', dist_from_the_sea: 70, has_tv: true})
+Hotel.create({name: "Диканька", city_name: 'Стрелково', dist_from_the_sea: 20, has_tv: false})
+
