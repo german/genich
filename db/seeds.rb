2eb456414bd997f29b529f421f46f6e74db05870
Vladimir Samojlov <v-moi@ya.ru>
2016-03-18 01:42:52 +0200

    update db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Hotel.delete_all
-Hotel.create({name: "Кобзарь", city_name: 'Геническ', dist_from_the_sea: 200, has_tv: true})
-Hotel.create({name: "Источник здоровья", city_name: 'Генгорка', dist_from_the_sea: 500, has_tv: true})
-Hotel.create({name: "Элит", city_name: 'Счастливцево', dist_from_the_sea: 50, has_tv: true})
-Hotel.create({name: "Комфорт Плюс", city_name: 'Счастливцево', dist_from_the_sea: 70, has_tv: true})
-Hotel.create({name: "Диканька", city_name: 'Стрелково', dist_from_the_sea: 20, has_tv: false})
+Hotel.create({name: "Кобзарь", city_name: 'Геническ', dist_from_the_sea: 200, has_tv: true, has_parking: true})
+Hotel.create({name: "Источник здоровья", city_name: 'Генгорка', dist_from_the_sea: 500, has_tv: true, has_parking: false})
+Hotel.create({name: "Элит", city_name: 'Счастливцево', dist_from_the_sea: 50, has_tv: true, has_parking: true})
+Hotel.create({name: "Комфорт Плюс", city_name: 'Счастливцево', dist_from_the_sea: 70, has_tv: true, has_parking: false})
+Hotel.create({name: "Диканька", city_name: 'Стрелково', dist_from_the_sea: 20, has_tv: false, has_parking: true})
 
