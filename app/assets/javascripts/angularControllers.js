a5424a994527872508f441d15f215421d24dd73d
Vladimir Samojlov <v-moi@ya.ru>
2016-03-28 14:27:16 +0300

    fix conflict

--- app/assets/javascripts/angularControllers.js
+++ app/assets/javascripts/angularControllers.js
@@ -1,24 +1,38 @@
 var pansionatApp = angular.module('pansionatApp', ['ngAnimate', 'ngRoute']);
+  
 
-pansionatApp.config(['$routeProvider', function($routeProvide){
-  $routeProvide
-    .when('/', {
-      templateUrl:'test.html', 
-      controller: 'PansListCrtl'
-    })
-}])
-
-pansionatApp.controller('PansListCrtl', function($scope, $http) {
+pansionatApp.controller('PansListCtrl', function($scope, $http, $route, $routeParams, $location) {
   $scope.hotels = [];
   $http.get('/hotels.json').success(function(data, status, headers, config){
     $scope.hotels = data;
   })
 
+  pansionatApp.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
+                when('/home', {templateUrl: 'index.html',   controller: PansListCtrl}).
+                otherwise({redirectTo: '/home'});
+  }]);
+
+  pansionatApp.controller('newCtrl', function($scope, $routeParams) {
+    $scope.name = "newCtrl";
+    $scope.params = $routeParams;
+  })
+  
   $scope.qty = 100;
   $scope.mycolor = 'blue';
 
-  // filters City
+  //ALL MY FILTERS MASSIVE
   $scope.city_nameIncludes = [];
+  $scope.has_tvIncludes = [];
+  $scope.has_parkingIncludes = [];
+  $scope.has_private_kitchenIncludes = [];
+  $scope.has_conditioningIncludes = [];
+  $scope.has_showerIncludes = [];
+  $scope.has_wifiIncludes = [];
+  $scope.has_transferIncludes = [];
+
+
+  // filters City
   $scope.includesCity_name = function(city_name){
     var i = $.inArray(city_name, $scope.city_nameIncludes);
     if (i > -1) {
@@ -27,7 +41,6 @@ pansionatApp.controller('PansListCrtl', function($scope, $http) {
       $scope.city_nameIncludes.push(city_name);
     }
   }
-
   $scope.citynameFilter = function(hotels) {
     if ($scope.city_nameIncludes.length > 0) {
       if ($.inArray(hotels.city_name, $scope.city_nameIncludes) < 0)
@@ -37,7 +50,6 @@ pansionatApp.controller('PansListCrtl', function($scope, $http) {
   }
 
   //filters has_tv
-  $scope.has_tvIncludes = [];
   $scope.includesHas_tv = function(has_tv){
     var i = $.inArray(has_tv, $scope.has_tvIncludes);
     if (i > -1) {
@@ -46,7 +58,6 @@ pansionatApp.controller('PansListCrtl', function($scope, $http) {
       $scope.has_tvIncludes.push(has_tv);
     }
   }
-
   $scope.hastvFilter = function(hotels) {
     if ($scope.has_tvIncludes.length > 0) {
       if ($.inArray(hotels.has_tv, $scope.has_tvIncludes) < 0)
@@ -59,7 +70,6 @@ pansionatApp.controller('PansListCrtl', function($scope, $http) {
   }
 
   //filters has_parking
-  $scope.has_parkingIncludes = [];
   $scope.includesHas_parking = function(has_parking){
     var i = $.inArray(has_parking, $scope.has_parkingIncludes);
     if (i > -1) {
@@ -68,7 +78,6 @@ pansionatApp.controller('PansListCrtl', function($scope, $http) {
       $scope.has_parkingIncludes.push(has_parking);
     }
   }
-
   $scope.hasparkingFilter = function(hotels) {
     if ($scope.has_parkingIncludes.length > 0) {
       if ($.inArray(hotels.has_parking, $scope.has_parkingIncludes) < 0)
@@ -81,7 +90,6 @@ pansionatApp.controller('PansListCrtl', function($scope, $http) {
   }
 
   //filters has_private_kitchen
-  $scope.has_private_kitchenIncludes = [];
   $scope.includeshas_private_kitchen = function(has_private_kitchen){
     var i = $.inArray(has_private_kitchen, $scope.has_private_kitchenIncludes);
     if (i > -1) {
@@ -90,7 +98,6 @@ pansionatApp.controller('PansListCrtl', function($scope, $http) {
       $scope.has_private_kitchenIncludes.push(has_private_kitchen);
     }
   }
-
   $scope.haskitchenFilter = function(hotels) {
     if ($scope.has_private_kitchenIncludes.length > 0) {
       if ($.inArray(hotels.has_private_kitchen, $scope.has_private_kitchenIncludes) < 0)
@@ -103,7 +110,6 @@ pansionatApp.controller('PansListCrtl', function($scope, $http) {
   }
 
   //filters has_conditioning
-  $scope.has_conditioningIncludes = [];
   $scope.includeshas_conditioning = function(has_conditioning){
     var i = $.inArray(has_conditioning, $scope.has_conditioningIncludes);
     if (i > -1) {
@@ -112,7 +118,6 @@ pansionatApp.controller('PansListCrtl', function($scope, $http) {
       $scope.has_conditioningIncludes.push(has_conditioning);
     }
   }
-
   $scope.hasairFilter = function(hotels) {
     if ($scope.has_conditioningIncludes.length > 0) {
       if ($.inArray(hotels.has_conditioning, $scope.has_conditioningIncludes) < 0)
@@ -120,6 +125,66 @@ pansionatApp.controller('PansListCrtl', function($scope, $http) {
         $('.air').addClass('air_active');
     } else {
       $('.air').removeClass('air_active');
+    }
+    return hotels;
+  }
+
+  //filters has_shower
+  $scope.includeshas_shower = function(has_shower){
+    var i = $.inArray(has_shower, $scope.has_showerIncludes);
+    if (i > -1) {
+      $scope.has_showerIncludes.splice(i, 1);
+    } else {
+      $scope.has_showerIncludes.push(has_shower);
+    }
+  }
+  $scope.hasshowerFilter = function(hotels) {
+    if ($scope.has_showerIncludes.length > 0) {
+      if ($.inArray(hotels.has_shower, $scope.has_showerIncludes) < 0)
+        return;
+        $('.shower').addClass('shower_active');
+    } else {
+      $('.shower').removeClass('shower_active');
+    }
+    return hotels;
+  }
+
+  //filters has_wifi
+  $scope.includeshas_wifi = function(has_wifi){
+    var i = $.inArray(has_wifi, $scope.has_wifiIncludes);
+    if (i > -1) {
+      $scope.has_wifiIncludes.splice(i, 1);
+    } else {
+      $scope.has_wifiIncludes.push(has_wifi);
+    }
+  }
+  $scope.haswifiFilter = function(hotels) {
+    if ($scope.has_wifiIncludes.length > 0) {
+      if ($.inArray(hotels.has_wifi, $scope.has_wifiIncludes) < 0)
+        return;
+        $('.wi-fi').addClass('wi-fi_active');
+    } else {
+      $('.wi-fi').removeClass('wi-fi_active');
+    }
+    return hotels;
+  }
+
+  //filters has_transfer
+  $scope.includeshas_transfer = function(has_transfer){
+    var i = $.inArray(has_transfer, $scope.has_transferIncludes);
+    if (i > -1) {
+      $scope.has_transferIncludes.splice(i, 1);
+    } else {
+      $scope.has_transferIncludes.push(has_transfer);
+    }
+  }
+  $scope.hastransferFilter = function(hotels) {
+    if ($scope.has_transferIncludes.length > 0) {
+      if ($.inArray(hotels.has_transfer, $scope.has_transferIncludes) < 0)
+        return;
+        $('.transfer').addClass('transfer_active');
+    } else {
+      $('.transfer').removeClass('transfer_active');
     }
     return hotels;
   }
@@ -141,10 +206,14 @@ pansionatApp.controller('PansListCrtl', function($scope, $http) {
     }
   }
 
-
   //filter premium
   $scope.premiumFilter = function(hotels, has_parking) {
     return hotels.has_tv === true;
+  }
+
+  //filter premium rendom functioun 
+  $scope.random = function() {
+    return 0.5 - Math.random();
   }
 
   $scope.isSortUp = function(fieldName) {
@@ -153,6 +222,4 @@ pansionatApp.controller('PansListCrtl', function($scope, $http) {
   $scope.isSortDown = function(fieldName) {
     return $scope.sortbyprice === fieldName && !$scope.reverse;
   };
-
-  
  })
--- app/assets/javascripts/myEffects.js
+++ app/assets/javascripts/myEffects.js
@@ -1,4 +1,4 @@
-$('.tv').click(function(){
+$('.tv').click(function(){  
 	console.log('work!');
 	var count_tv_click = 0
 	if(count_tv_click == 0) {
@@ -7,4 +7,33 @@ $('.tv').click(function(){
 	} else {
 		$('.tv').removeClass('tv_active');
 	}
+})
+
+$(function(){
+ var shrinkHeader = 805;
+	$(window).scroll(function() {
+		var scroll = getCurrentScroll();
+		if ( scroll >= shrinkHeader ) {
+			$('.arrowsup').fadeIn('fast');
+		} else {
+			$('.arrowsup').fadeOut('fast');
+		}
+	});
+	function getCurrentScroll() {
+		return window.pageYOffset || document.documentElement.scrollTop;
+	}
+});
+
+$('.arrowsup').click(function () {
+	console.log('Work!')
+	$('body').animate({
+	scrollTop: 0
+	}, 800);
+	return false;
+});
+
+
+// НЕ РАБОТАЕТ CLICK
+$('body').click(function(){
+	console.log('YES')
 })
