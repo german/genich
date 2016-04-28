8eacb945cc22fe0e2ca15d044ff4405a1ce44921
Vladimir Samojlov <v-moi@ya.ru>
2016-04-28 12:18:58 +0300

    login

--- app/assets/javascripts/controllers/login.controller.js
+++ app/assets/javascripts/controllers/login.controller.js
@@ -1,4 +1,4 @@
-angular.module('pansionatApp').controller('LoginCtrl', function($scope, $stateParams, $http, Auth) {
+angular.module('pansionatApp').controller('LoginCtrl', function($scope, $stateParams, $http, Auth, $state) {
   $scope.save = function() {
     var credentials = {
       email: $scope.email,
@@ -10,19 +10,23 @@ angular.module('pansionatApp').controller('LoginCtrl', function($scope, $statePa
       }
     };
 
+
     Auth.login(credentials, config).then(function(user) {
       console.log(user); // => {id: 1, ect: '...'}
+      $state.go('Main');
     }, function(error) {
       // Authentication failed...
     });
 
     $scope.$on('devise:login', function(event, currentUser) {
       console.log('currentUser', currentUser);
+      $state.go('Main');
       // after a login, a hard refresh, a new tab
     });
 
     $scope.$on('devise:new-session', function(event, currentUser) {
       // user logged in by Auth.login({...})
+
     });
   }
 })
--- app/assets/javascripts/controllers/registration.controller.js
+++ app/assets/javascripts/controllers/registration.controller.js
@@ -1,10 +1,10 @@
 angular.module('pansionatApp').controller('RegistrationCtrl', function($scope, $stateParams, $http, $state) {
   $scope.save = function() {
-    if($scope.email != "" && $scope.password != "" && $scope.password_confirmation != "") {
+    if($scope.email && $scope.password && $scope.password_confirmation) {
 	    $http.post('/users', {user: {email: $scope.email, password: $scope.password, password_confirmation: $scope.password_confirmation}})
 	      .success(function(data, status, headers, config){
 	        console.log(data);
-	        $state.go('home');
+	        $state.go('login');
 	    });
 	  } else {
 	  	alert('Заполните все поля')
