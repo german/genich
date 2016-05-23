cbe2e400c65aaacf63a694959e9306fdf4b45134
Dmytro Samoilov <e2718281828@ya.ru>
2016-05-23 16:34:53 +0300

    added roles saving on BE

--- app/assets/javascripts/controllers/registration.controller.js
+++ app/assets/javascripts/controllers/registration.controller.js
@@ -1,13 +1,14 @@
 angular.module('pansionatApp').controller('RegistrationCtrl', function($scope, $stateParams, $http, $state) {
   $scope.save = function() {
-  	console.log($scope.user.$valid)
+    console.log($scope.user.$valid);
     if ($scope.user.$valid == true)
     	// ($scope.email && $scope.password && $scope.password_confirmation)
     	 {
 	    $http.post('/users', {user: {
 	    	email: $scope.email, 
 	    	password: $scope.password, 
-	    	password_confirmation: $scope.password_confirmation
+	    	password_confirmation: $scope.password_confirmation,
+        role: $scope.role
 	    }})
 	      .success(function(data, status, headers, config){
 	        console.log(data);
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,21 @@
+class RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters, :only => [:create]
+
+  #def create
+  #  super
+    # could be changed later to #deliver_later
+  #  UsersMailer.welcome(@user).deliver_now unless @user.invalid?
+  #end
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :role, :password_confirmation) }
+  end
+
+private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :email, :password, :password_confirmation, :role)
+  end
+end
