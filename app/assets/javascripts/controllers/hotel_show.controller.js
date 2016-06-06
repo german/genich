85ef4b314412ce81f5a2149092444523dd2e0658
Vladimir Samojlov <v-moi@ya.ru>
2016-06-06 15:54:44 +0300

    xeditable3

--- app/assets/javascripts/controllers/hotel_show.controller.js
+++ app/assets/javascripts/controllers/hotel_show.controller.js
@@ -29,7 +29,7 @@ angular.module('pansionatApp').run(function(editableOptions){editableOptions.the
   
   $scope.updateAlbumname = function() {
     Album.save({hotel_id: $stateParams.id, album: $scope.newAlbum }, function(response) {
-      console.log('edit');
+      console.log(response);
     })
   };
 
