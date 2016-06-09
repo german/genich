97d0b9b7b947968b7b7c668a8e99a7683869b13c
Vladimir Samojlov <v-moi@ya.ru>
2016-06-09 23:32:08 +0300

    profile and help

--- app/assets/javascripts/controllers/user.controller.js
+++ app/assets/javascripts/controllers/user.controller.js
@@ -2,8 +2,8 @@ angular.module('pansionatApp').controller('UserCtrl',
 	function($scope, $stateParams, User, $http, $localStorage, $sessionStorage) { 
 		$scope.myuser = JSON.parse(window.localStorage.getItem('currentUser'));
 
-		$scope.updateUser = function(myuser_id) {
-    	User.update({id: myuser_id, myuser_email: $scope.edit_myuser.email }, function(response) {
+		$scope.updateUser = function(myuser_id, credentials) {
+    	User.update({id: myuser_id, myuser_email: $scope.myuser.email }, function(response) {
       	console.log(response);
     	})
   	};
