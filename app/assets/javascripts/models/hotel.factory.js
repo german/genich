7dbc5ac67e7e99fa6ed2ef27f6b3f0dad55b8502
Dmitrii Samoilov <e2718281828@ya.ru>
2016-06-12 18:34:15 +0300

    update hotels

--- app/assets/javascripts/models/hotel.factory.js
+++ app/assets/javascripts/models/hotel.factory.js
@@ -1,7 +1,13 @@
 angular.module('pansionatApp').factory('Hotel', function($resource) {
   return $resource('/hotels/:id', { id: '@id'},
   {
-   'update': { method: 'PUT', url: '/hotels/:id'},
+   'update': { method: 'PUT', 
+     url: '/hotels/:id',
+     headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+   },
    'save': { 
       method: 'POST', 
       headers: {
--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -30,9 +30,16 @@ class HotelsController < ApplicationController
     end
   end
 
+  def update
+    update! do |success, failure|
+      success.json { render json: @hotel }
+      failure.json { render json: @hotel.errors, status: 422 }
+    end
+  end
+
 protected
 	def hotel_params
-		params.require(:hotel).permit(:name, :city_name, :has_parking, :total_rooms, :description, :email,
+		params.require(:hotel).permit(:id, :hotel_type, :has_pool, :albums, :reviews, :type, :name, :city_name, :has_parking, :total_rooms, :description, :email,
       :phones, :total_luxes, :rules, :people_in_room, :price_min, :price_mean, :price_max, :longitude, 
       :latitude, :dist_from_the_sea, :has_private_house, :has_private_kitchen, :has_private_shower, 
       :has_fridge, :has_tv, :has_conditioning, :has_wifi, :has_transfer, :user_id)
